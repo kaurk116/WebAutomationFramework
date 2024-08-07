348cee2cb799b69b0278aa696ecc020ebc4e5664
karamjeet <kaurk161@gmail.com>
2024-08-07 17:47:22 +0530

    again chrome

--- src/main/java/com/thetestingacademy/Divers/DriverManager.java
+++ src/main/java/com/thetestingacademy/Divers/DriverManager.java
@@ -1,9 +1,14 @@
 package com.thetestingacademy.Divers;
 
+import com.thetestingacademy.Utils.PropertyReader;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+
+import java.io.FileNotFoundException;
 
 public class DriverManager {
     public static WebDriver driver;  //add getter setter
@@ -16,18 +21,37 @@ public class DriverManager {
         DriverManager.driver = driver;
     }
 
-    public  static void init(){
-        if(driver == null){
-            EdgeOptions edgeOptions =new EdgeOptions();
-            edgeOptions.addArguments("--guest");
-            edgeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-             driver =new EdgeDriver(edgeOptions);
+    public static void init() {
+
+        String browser = null;
+        try {
+            browser = PropertyReader.readKey("browser");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        if( driver == null){
+        switch (browser) {
+            case "edge":
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--guest");
+                driver = new EdgeDriver(edgeOptions);
+                break;
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--start-maximized");
+                driver = new ChromeDriver(chromeOptions);
+                break;
+            default:
+                System.out.println("Not browser Found!!");
         }
     }
-    public static  void close(){
-        if(driver!= null);
-        driver.quit();
-        driver = null;
-
-    }
 }
+
+        public static void close () {
+            if (driver != null) ;
+            driver.quit();
+            driver = null;
+
+        }
+    }
+
