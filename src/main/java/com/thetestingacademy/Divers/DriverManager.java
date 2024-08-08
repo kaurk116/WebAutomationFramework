d228780e5a073cef649db117375078894cb09246
karamjeet <kaurk161@gmail.com>
2024-08-08 16:31:40 +0530

    Driver Manager

--- src/main/java/com/thetestingacademy/Divers/DriverManager.java
+++ src/main/java/com/thetestingacademy/Divers/DriverManager.java
@@ -2,6 +2,7 @@ package com.thetestingacademy.Divers;
 
 import com.thetestingacademy.Utils.PropertyReader;
 import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.SessionNotCreatedException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -22,30 +23,39 @@ public class DriverManager {
     }
 
     public static void init() {
-
         String browser = null;
         try {
             browser = PropertyReader.readKey("browser");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        if( driver == null){
-        switch (browser) {
-            case "edge":
-                EdgeOptions edgeOptions = new EdgeOptions();
-                edgeOptions.addArguments("--guest");
-                driver = new EdgeDriver(edgeOptions);
-                break;
-            case "chrome":
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--start-maximized");
-                driver = new ChromeDriver(chromeOptions);
-                break;
-            default:
-                System.out.println("Not browser Found!!");
+
+        if (driver == null) {
+            try {
+                switch (browser) {
+                    case "edge":
+                        EdgeOptions edgeOptions = new EdgeOptions();
+                        edgeOptions.addArguments("--guest");
+                        edgeOptions.setBinary("/usr/bin/microsoft-edge");
+                        driver = new EdgeDriver(edgeOptions);
+
+                        //edgeOptions.binary_location = "/usr/bin/microsoft-edge";
+                        break;
+                    case "chrome":
+                        ChromeOptions chromeOptions = new ChromeOptions();
+                        chromeOptions.addArguments("--start-maximized");
+                        driver = new ChromeDriver(chromeOptions);
+                        break;
+                    default:
+                        System.out.println("Not browser Found!!");
+                }
+            } catch (SessionNotCreatedException e) {
+                System.out.println("Failed to create a new session: " + e.getMessage());
+                e.printStackTrace();
+                // Add additional logic to handle the exception, such as retrying or logging the issue
+            }
         }
     }
-}
 
         public static void close () {
             if (driver != null) ;
